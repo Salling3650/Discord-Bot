6f3387584ebaade5f9398d51144e75982e6d86b2
Frederik <94721158+Salling3650@users.noreply.github.com>
2021-12-27 10:42:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 import os
 from keep_alive import keep_alive
 import requests
@@ -24,7 +23,13 @@ async def on_message(message):
 
   if message.content.startswith('$inspire'):
     quote = get_quote()
-    await message.channel.send(quote)
+    await message.channel.send(quote) 
+ 
+@bot.event
+async def on_message(message):
+    if(message.channel.id == 924952660862435338):
+        await bot.add_reaction(message,
+        924955343925149736, 924955456802271252)
 
 #Deez command start
   if message.content.startswith("Deez"):
@@ -50,7 +55,7 @@ async def on_message(message):
   if message.content.startswith("TSP is a god"):
     Yes = "https://tenor.com/view/breaking-bad-walter-white-youre-goddamn-right-gif-14600753"
     await message.channel.send(Yes)
-#Deez command end
+#TSP_is_a_god command end
 
 #Tank command start
   if message.content.startswith("!tank"):
@@ -58,9 +63,27 @@ async def on_message(message):
     await message.channel.send(Command)
 #Tank command end
 
+#Fuck command start
+  if message.content.startswith("!fuck"):
+    Fuck = "Fuck, Refuses to elaborate, Refuses to leave"
+    await message.channel.send(Fuck)
+#Fuck command end
+
+#Potat_is_a_god command start
+  if message.content.startswith("Potat is a god"):
+    NO = "https://tenor.com/view/religion-bill-wurtz-dont-history-gif-14178134"
+    await message.channel.send(NO)
+#Potat_is_a_god command end
+
+#The_cake_is_a_lie command start
+  if message.content.startswith("The cake is a lie"):
+    cake = "https://tenor.com/view/cake-lie-the-cake-is-a-lie-cake-is-a-lie-gif-19992579"
+    await message.channel.send(cake)
+#The_cake_is_a_lie command end
+
 #Command command start
   if message.content.startswith("!command"):
-    Command = "Deez, Ligma, TSP is a god, !tank"
+    Command = "Deez, Ligma, TSP is a god, !tank, !fuck"
     await message.channel.send(Command)
 #Command command end
 
